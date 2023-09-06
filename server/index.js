a4cff188d83e34d83107e98c084c7b3e32ebe3e9
mukulembezewil <wilmukulembeze@gmail.com>
2023-09-06 13:31:04 +0300

    Edit in db connection in to server.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+	mongoose
+		.connect(process.env.MONGO_URI)
+		.then(() => {
+			console.log('Hefty congs! We are hooked up to some Mongo DB!');
+		})
+		.catch((err) => {
+			throw err;
+		});
+};
 
 app.listen(8800, () => {
+	connect();
 	console.log('Server connected and listening on 8800');
 });
