01b775cb911cb202ed261c7639150221ba08984a
mukulembezewil <wilmukulembeze@gmail.com>
2023-09-07 13:36:43 +0300

    Install & import jason web token

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import Viduser from '../models/Viduser.js';
 import bcrypt from 'bcrypt';
 import { createError } from '../error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
 	try {
@@ -11,6 +12,29 @@ export const signup = async (req, res, next) => {
 
 		await newVidUser.save();
 		res.status(200).send('Success! Video user has been created!');
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const signin = async (req, res, next) => {
+	try {
+		const user = await Viduser.findOne({ name: req.body.name });
+		if (!user) return next(createError(404, 'User no where to be found!'));
+
+		const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+		if (!isCorrect) return next(createError(400, 'Invalid credentials Pal!'));
+
+		const token = jwt.sign({ id: user._id }, process.env.JWT);
+		const { password, ...others } = user._doc;
+
+		res
+			.cookie('access_token', token, {
+				httpOnly: true,
+			})
+			.status(200)
+			.json(others);
 	} catch (err) {
 		next(err);
 	}
