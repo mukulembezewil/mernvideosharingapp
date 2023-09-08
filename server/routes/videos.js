14da570ad09e910fd153dc519432c0b74df67b24
mukulembezewil <wilmukulembeze@gmail.com>
2023-09-08 22:34:54 +0300

    Edit comments and video routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import {
 	addVideo,
+	addView,
 	deleteVideo,
+	getByTag,
 	getVideo,
+	random,
+	search,
+	sub,
+	trend,
 	updateVideo,
 } from '../controllers/video.js';
 import { verifyToken } from '../verifyToken.js';
@@ -13,9 +19,11 @@ router.post('/', verifyToken, addVideo);
 router.put('/:id', verifyToken, updateVideo);
 router.delete('/:id', verifyToken, deleteVideo);
 router.get('/find/:id', getVideo);
-router.put('/view/:id', addVideo);
-router.get('/trend', addVideo);
-router.get('/random', addVideo);
-router.get('/sub', addVideo);
+router.put('/view/:id', addView);
+router.get('/trend', trend);
+router.get('/random', random);
+router.get('/sub', sub);
+router.get('/tags', getByTag);
+router.get('/search', search);
 
 export default router;
