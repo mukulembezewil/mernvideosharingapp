447ec5829a6aea68c8ff54da2f5df764e5e5655e
mukulembezewil <wilmukulembeze@gmail.com>
2023-09-08 04:37:40 +0300

    Add to video routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,21 @@
 import express from 'express';
-import {} from '../controllers/video.js';
+import {
+	addVideo,
+	deleteVideo,
+	getVideo,
+	updateVideo,
+} from '../controllers/video.js';
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
+
+router.post('/', verifyToken, addVideo);
+router.put('/:id', verifyToken, updateVideo);
+router.delete('/:id', verifyToken, deleteVideo);
+router.get('/find/:id', getVideo);
+router.put('/view/:id', addVideo);
+router.get('/trend', addVideo);
+router.get('/random', addVideo);
+router.get('/sub', addVideo);
 
 export default router;
