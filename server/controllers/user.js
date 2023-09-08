ff4e695b75d30eefbea9cd7c907b4c2cf5b96c2c
mukulembezewil <wilmukulembeze@gmail.com>
2023-09-08 03:32:24 +0300

    Wire in getUser, subscribe and unsubscribe request handlers.

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -37,12 +37,49 @@ export const deleteUser = async (req, res, next) => {
 	}
 };
 
-export const getUser = async (req, res) => {};
+export const getUser = async (req, res, next) => {
+	try {
+		const user = await Viduser.findById(req.params.id);
+		res.status(200).json(user);
+	} catch (err) {
+		next(err);
+	}
+};
 
-export const subscribe = async (req, res) => {};
+export const subscribe = async (req, res, next) => {
+	try {
+		await Viduser.findById(req.user.id, {
+			$push: { subscribedUsers: req.params.id },
+		});
+		await Viduser.findByIdAndUpdate(req.params.id, { $inc: { subscribers: 1 } });
+		res.status(200).json('Success! Subscriber added!');
+	} catch (err) {
+		next(err);
+	}
+};
 
-export const unsubscribe = async (req, res) => {};
+export const unsubscribe = async (req, res, next) => {
+	try {
+		await Viduser.findById(req.user.id, {
+			$pull: { subscribedUsers: req.params.id },
+		});
+		await Viduser.findByIdAndUpdate(req.params.id, { $inc: { subscribers: -1 } });
+		res.status(200).json('Subscriber successfully removed!');
+	} catch (err) {
+		next(err);
+	}
+};
 
-export const like = async (req, res) => {};
+export const like = async (req, res, next) => {
+	try {
+	} catch (err) {
+		next(err);
+	}
+};
 
-export const dislike = async (req, res) => {};
+export const dislike = async (req, res, next) => {
+	try {
+	} catch (err) {
+		next(err);
+	}
+};
