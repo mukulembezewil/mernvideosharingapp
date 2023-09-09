cf08dfb7f8d640b8ee63955fcd04b59262eeb7b8
mukulembezewil <wilmukulembeze@gmail.com>
2023-09-09 13:31:04 +0300

    Add dependencies

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,38 +1,38 @@
-import React from "react";
-import styled from "styled-components";
-import Card from "../components/Card";
+import React from 'react';
+import styled from 'styled-components';
+import Card from '../components/Card';
 
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: wrap;
 `;
 
 const Home = () => {
-  return (
-    <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </Container>
-  );
+	return (
+		<Container>
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+		</Container>
+	);
 };
 
 export default Home;
