166198e0862184ea858ea0fd7abe24c918660df1
mukulembezewil <wilmukulembeze@gmail.com>
2023-09-16 03:07:58 +0300

    Add Explore and Trend links

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -99,14 +99,24 @@ const Menu = ({ darkMode, setdarkMode }) => {
 					<HomeIcon />
 					Home
 				</Item>
-				<Item>
-					<ExploreIcon />
-					Explore
-				</Item>
-				<Item>
-					<SubscriptionsIcon />
-					Subscriptions
-				</Item>
+				<Link
+					to="trends"
+					style={{ textDecoration: 'none', color: 'inherit' }}
+				>
+					<Item>
+						<ExploreIcon />
+						Explore
+					</Item>
+				</Link>
+				<Link
+					to="subscriptions"
+					style={{ textDecoration: 'none', color: 'inherit' }}
+				>
+					<Item>
+						<SubscriptionsIcon />
+						Subscriptions
+					</Item>
+				</Link>
 				<Hr />
 				<Item>
 					<VideoLibraryIcon />
