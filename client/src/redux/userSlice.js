f56b3d5c930a19b52261cb49ce23251673cd2f61
mukulembezewil <wilmukulembeze@gmail.com>
2023-09-25 15:28:56 +0300

    Add to User Slice

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -24,10 +24,22 @@ export const userSlice = createSlice({
 		logout: (state) => {
 			return initialState;
 		},
+		subscription: (state, action) => {
+			if (state.currentuser.subscribedUsers.includes(action.payload)) {
+				state.currentuser.subscribedUsers.splice(
+					state.currentuser.subscribedUsers.findIndex(
+						(channelId) => channelId === action.payload
+					),
+					1
+				);
+			} else {
+				state.currentuser.subscribedUsers.push(action.payload);
+			}
+		},
 	},
 });
 
-export const { loginStart, loginSuccess, loginFailure, logout } =
+export const { loginStart, loginSuccess, loginFailure, logout, subscription } =
 	userSlice.actions;
 
 export default userSlice.reducer;
